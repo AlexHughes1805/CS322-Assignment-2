5041f2ef515100519f08e20b62cbcc26b6895ec3
Alex H <alexhughes1805@gmail.com>
2025-11-19 12:36:38 +0000

    filter sliders - to fix

--- script.js
+++ script.js
@@ -6,17 +6,41 @@ const bandpassCheck = document.getElementById("bandpass");
 const highshelfCheck = document.getElementById("highshelf");
 const lowshelfCheck = document.getElementById("lowshelf");
 
+var highpassSlide = document.getElementById("highpassGain");
+var highpassGain = highpassSlide.value;
+var lowpassSlide = document.getElementById("lowpassGain");
+var lowpassGain = lowpassSlide.value;
+var bandpassSlide = document.getElementById("bandpassGain");
+var bandpassGain = bandpassSlide.value;
+var highshelfSlide = document.getElementById("highshelfGain");
+var highshelfGain = highshelfSlide.value;
+var lowshelfSlide = document.getElementById("lowshelfGain");
+var lowshelfGain = lowshelfSlide.value;
+
+var output = document.getElementById("demo");
+output.innerHTML = lowpassSlide.value;
+
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const source = audioCtx.createMediaElementSource(audio);
 const analyser = audioCtx.createAnalyser();
 const distortion = audioCtx.createWaveShaper();
 const gainNode = audioCtx.createGain();
-const biquadFilter = audioCtx.createBiquadFilter();
+
+// handle each filter independently of each other
+const highpassFilter = audioCtx.createBiquadFilter(); highpassFilter.type = "allpass";
+const lowpassFilter  = audioCtx.createBiquadFilter(); lowpassFilter.type  = "allpass";
+const bandpassFilter = audioCtx.createBiquadFilter(); bandpassFilter.type = "allpass";
+const lowshelfFilter = audioCtx.createBiquadFilter(); lowshelfFilter.type = "allpass";
+const highshelfFilter = audioCtx.createBiquadFilter(); highshelfFilter.type = "allpass";
 
 source.connect(analyser);
 analyser.connect(distortion);
-distortion.connect(biquadFilter);
-biquadFilter.connect(gainNode);
+distortion.connect(highpassFilter);
+highpassFilter.connect(lowpassFilter);
+lowpassFilter.connect(bandpassFilter);
+bandpassFilter.connect(lowshelfFilter);
+lowshelfFilter.connect(highshelfFilter);
+highshelfFilter.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 
 // Handle file selection
@@ -35,48 +59,59 @@ fileInput.addEventListener("change", (event) => {
     }
 });
 
-// enable highpass filter when highpass checkbox is ticked
+// enable highpass filter when highpass checkbox is ticked and so on and so forth
 highpassCheck.addEventListener("change", () => {
     if (highpassCheck.checked) {
-        biquadFilter.type = "highpass";
+        highpassFilter.type = "highpass";
+        highpassFilter.gain.value = highpassGain;
     }
     else{
-        biquadFilter.type = "";
+        highpassFilter.type = "allpass";
     }
 });
 
 lowpassCheck.addEventListener("change", () => {
-    if (highpassCheck.checked) {
-        biquadFilter.type = "lowpass";
+    lowpassSlide.oninput = function() {
+        output.innerHTML = this.value;
+    }
+    output.innerHTML = lowpassSlide.value;
+    if (lowpassCheck.checked) {
+        lowpassFilter.type = "lowpass";
     }
     else{
-        biquadFilter.type = "";
+        lowpassFilter.type = "allpass";
     }
 });
 
 bandpassCheck.addEventListener("change", () => {
-    if (highpassCheck.checked) {
-        biquadFilter.type = "bandpass";
+    if (bandpassCheck.checked) {
+        bandpassFilter.type = "bandpass";
+        bandpassFilter.gain.value = bandpassGain;
     }
     else{
-        biquadFilter.type = "";
+        bandpassFilter.type = "allpass";
     }
 });
 
 lowshelfCheck.addEventListener("change", () => {
-    if (highpassCheck.checked) {
-        biquadFilter.type = "lowshelf";
+    if (lowshelfCheck.checked) {
+        lowshelfFilter.type = "lowshelf";
     }
     else{
-        biquadFilter.type = "";
+        lowshelfFilter.type = "allpass";
+    }
+    while(checked == true)
+    {
+        console.log(lowpassFilter.gain.value);
     }
 });
 
 highshelfCheck.addEventListener("change", () => {
-    if (highpassCheck.checked) {
-        biquadFilter.type = "highshelf";
+    if (highshelfCheck.checked) {
+        highshelfFilter.type = "highshelf";
+        highshelfFilter.gain.value = highshelfGain;
     }
     else{
-        biquadFilter.type = "";
+        highpassFilter.type = "allpass";
     }
 });
